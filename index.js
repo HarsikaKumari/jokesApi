15c8d1f194730679a69da5e6359f1bb68adb0916
Harsika kumari <kumariharsika8@gmail.com>
2024-02-04 19:06:26 +0530

    compeleted the project

--- index.js
+++ index.js
@@ -6,10 +6,42 @@ import exp from "constants";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res)=>{
-    res.render("index.js", {content:"waiting for the data..."})
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://v2.jokeapi.dev/joke/Any");
+    const result = response.data;
+    console.log(response);
+    res.render("index.ejs", {data: result});
+  } catch (error) {
+    console.log("Failed to make request: ", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
-app.listen(port, (req, res) =>{
-    console.log(`The server is running on port ${port}`);
+app.post("/", async (req, res) => {
+  const string = req.body.search_string;
+  try {
+    console.log(req.body);
+    const flags = req.body.flags;
+    const categories = req.body.category / categories;
+    const language = req.body.language;
+    const response = await axios.get(
+      `https://v2.jokeapi.dev/joke/${categories}/lang=${language}?blacklistFlags=${flags}`
+    );
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    res.render("index.ejs", {
+      error: `Oops!! looks like we don't have jokes which contain ${string}`,
+    });
+  }
+});
+
+app.listen(port, (req, res) => {
+  console.log(`The server is running on port ${port}`);
 });
